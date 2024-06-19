eb08ac032cacaedcaa46c43429a76091b8f518ca
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-06-19 20:00:07 +0530

    dynamically created a ticket

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const modalCont=document.querySelector('.modal-cont');
 const addBtn=document.querySelector('.add-btn');
+const mainCont=document.querySelector('.main-cont');
+const textarea=document.querySelector('.text-area-cont')
 
 let addTaskFlag = false;
 modalCont.style.display='none';
@@ -21,5 +23,34 @@ addBtn.addEventListener('click',function(e)
             console.log(addTaskFlag);
 
         }
+
+});
+
+function CreateTicket()
+{
+    const div=document.createElement('div');
+    div.classList.add("ticket-cont");
+    div.innerHTML=
+    `<div class="ticket-color">blue</div>
+            <div class="ticket-id">123</div>
+            <div class="task-area" contenteditable>random 1st task</div>
+            <div class="ticket-lock">
+                <i class="fa-solid fa-lock"></i>
+            </div>`
+
+    mainCont.appendChild(div);
+
+
+}
+
+modalCont.addEventListener('keydown', function(e){
+   if(e.shiftKey)
+    {
+        CreateTicket();
+        console.log("swapna");
+        modalCont.style.display='none';
+        textarea.value="";
+
+    }
 
 });
