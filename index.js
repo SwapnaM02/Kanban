b48decbf5a20e0856a1f179656ca5400921bbb0d
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-06-22 10:39:57 +0530

    changed the background color of removebtn, on activate red

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const modalCont=document.querySelector('.modal-cont');
 const addBtn=document.querySelector('.add-btn');
 const mainCont=document.querySelector('.main-cont');
 const textarea=document.querySelector('.text-area-cont')
+const prioritycolor=document.querySelectorAll('.priority-color');
+const removeBtn=document.querySelector('.remove-btn');
+
+
+
+
 
 let addTaskFlag = false;
 modalCont.style.display='none';
+let selectedcolor='black';
+let removeTaskFlag='false';
 
 
 addBtn.addEventListener('click',function(e)
@@ -26,14 +34,14 @@ addBtn.addEventListener('click',function(e)
 
 });
 
-function CreateTicket()
+function CreateTicket(selectedcolor,text,id)
 {
     const div=document.createElement('div');
     div.classList.add("ticket-cont");
     div.innerHTML=
-    `<div class="ticket-color">blue</div>
-            <div class="ticket-id">123</div>
-            <div class="task-area" contenteditable>random 1st task</div>
+    `<div class="ticket-color" style=background-color:${selectedcolor}>${selectedcolor}</div>
+            <div class="ticket-id">${id}</div>
+            <div class="task-area" contenteditable>${text}</div>
             <div class="ticket-lock">
                 <i class="fa-solid fa-lock"></i>
             </div>`
@@ -43,14 +51,49 @@ function CreateTicket()
 
 }
 
-modalCont.addEventListener('keydown', function(e){
+window.addEventListener('keydown', function(e){
    if(e.shiftKey)
     {
-        CreateTicket();
+        const id=Math.ceil(Math.random()*100).toString();
+        CreateTicket(selectedcolor,textarea.value,id);
         console.log("swapna");
         modalCont.style.display='none';
         textarea.value="";
 
+
     }
 
 });
+
+
+prioritycolor.forEach(color => color.addEventListener('click', function(e){
+    prioritycolor.forEach(colordiv => {
+        colordiv.classList.remove('active');
+    })
+
+
+    color.classList.add('active');
+    selectedcolor=color.classList[0];
+    console.log(selectedcolor);
+    
+
+}));
+
+removeBtn.addEventListener('click',function()
+{
+    removeTaskFlag=!removeTaskFlag;
+    if(removeTaskFlag)
+        {
+            alert('Delete btn activated');
+            removeBtn.style.color="red";
+
+        }
+
+        else{
+            removeBtn.style.color="white";
+
+        }
+})
+
+
+
